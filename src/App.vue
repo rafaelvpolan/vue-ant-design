9b2d17aadbb25768ead19d03e61d03a3b1f9a415
Rafael Vergo Polan <rafaelvpolan@gmail.com>
2019-05-20 03:34:01 -0300

    Implementado Vue-Antd

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+ <a-locale-provider :locale="locale">
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+  </a-locale-provider>
+</template>
+
+<script>
+
+import ptBR from 'ant-design-vue/lib/locale-provider/pt_BR';
+
+import EventBus from '@/utils/eventBus';
+
+
+export default {
+  data() {
+    return {
+      locale: ptBR
+    }
+  },
+  created(){
+    EventBus.$on('CHANGE_LOCALE',(lang)=>{
+    
+      this.locale=(lang?lang:ptBR)
+    
+    });
+  }
+
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Vue + Antd"/>
+
+    <a-button type="primary">Primary</a-button> &nbsp;
+    <a-button>Default</a-button> &nbsp;
+    <a-button type="dashed">Dashed</a-button> &nbsp; 
+    <a-button type="danger">Danger</a-button> 
+    <br>
+    <br>
+
+
+<a-month-picker /> &nbsp;
+    <a-select defaultValue="pt_BR" style="width: 120px" @change="handleChange">
+      <a-select-option value="pt_BR" >Português</a-select-option>
+      <a-select-option value="en_GB" >Inglês</a-select-option>
+      <a-select-option value="es_ES" >Espanhol</a-select-option>
+    </a-select>
+
+
+ 
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import EventBus from '@/utils/eventBus'
+
+import ptBR from 'ant-design-vue/lib/locale-provider/pt_BR';
+import enGB from 'ant-design-vue/lib/locale-provider/en_GB';
+import esES from 'ant-design-vue/lib/locale-provider/es_ES';
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      langs:{
+        pt_BR: ptBR,
+        en_GB: enGB,
+        es_ES: esES
+      }
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods:{
+    handleChange(value){
+      EventBus.$emit('CHANGE_LOCALE',this.langs[value])
+    }
+  }
+}
+</script>
